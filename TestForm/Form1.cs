167d0bd4f517842299e70bad76f2dfdb7b6bbdc0
wenysky <wenysky@b489ef22-979d-11dd-bbfe-3dabce05a288>
2008-11-13 14:49:38 +0000

    测试项目入库

---
+++ TestForm/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TestForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelectTemplateFolder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                tbxTemplateFolder.Text = fbd.SelectedPath;
+            }
+        }
+
+        private void btnSelectPagefileFolder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                tbxPagefileFolder.Text = fbd.SelectedPath;
+            }
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
