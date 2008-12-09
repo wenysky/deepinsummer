5a1520db530c73db9cd1f88a702eeaff03473601
wenysky <wenysky@b489ef22-979d-11dd-bbfe-3dabce05a288>
2008-12-09 05:02:26 +0000

    命名空间和类名重新整理

---
+++ DeepinSummer/Common/Utils.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Natsuhime.Common
+{
+    public class Utils
+    {
+        /// <summary>
+        /// 是否为ip
+        /// </summary>
+        /// <param name="ip"></param>
+        /// <returns></returns>
+        public static bool IsIP(string ip)
+        {
+            return Regex.IsMatch(ip, @"^((2[0-4]\d|25[0-5]|[01]?\d\d?)\.){3}(2[0-4]\d|25[0-5]|[01]?\d\d?)$");
+        }
+
+        /// <summary>
+        /// 取得当前的时间戳.
+        /// </summary>
+        /// <returns></returns>
+        public static string UnixTimestamp()
+        {
+            DateTime dtStart = TimeZone.CurrentTimeZone.ToLocalTime(new DateTime(1970, 1, 1));
+            DateTime dtNow = DateTime.Parse(DateTime.Now.ToString());
+            TimeSpan toNow = dtNow.Subtract(dtStart);
+            string timeStamp = toNow.Ticks.ToString();
+            return timeStamp.Substring(0, timeStamp.Length - 7);
+        }
+    }
+}
