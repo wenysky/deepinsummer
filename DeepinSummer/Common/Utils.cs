07513d8fd6861221049a470d56374db96adc974b
wenysky <wenysky@b489ef22-979d-11dd-bbfe-3dabce05a288>
2008-12-12 08:37:50 +0000

    数据库连接信息从Web.config移到NSWindBase.config中

--- DeepinSummer/Common/Utils.cs
+++ DeepinSummer/Common/Utils.cs
@@ -26,6 +26,28 @@ namespace Natsuhime.Common
         {
             return !Regex.IsMatch(str, @"[-|;|,|\/|\(|\)|\[|\]|\}|\{|%|@|\*|!|\']");
         }
+
+        /// <summary>
+        /// 获得当前绝对路径
+        /// </summary>
+        /// <param name="strPath">指定的路径</param>
+        /// <returns>绝对路径</returns>
+        public static string GetMapPath(string strPath)
+        {
+            if (System.Web.HttpContext.Current != null)
+            {
+                return System.Web.HttpContext.Current.Server.MapPath(strPath);
+            }
+            else //非web程序引用
+            {
+                strPath = strPath.Replace("/", "\\");
+                if (strPath.StartsWith("\\"))
+                {
+                    strPath = strPath.Substring(strPath.IndexOf('\\', 1)).TrimStart('\\');
+                }
+                return System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, strPath);
+            }
+        }
         /// <summary>
         /// 取得当前的时间戳.
         /// </summary>
--- DeepinSummer/Data/DBConfigs.cs
+++ DeepinSummer/Data/DBConfigs.cs
@@ -1,12 +1,69 @@
 using System;
-using System.Collections.Generic;
-using System.Text;
+using Natsuhime.Common;
 using System.Configuration;
 
 namespace Natsuhime.Data
 {
-    public class DBConfigs
+    public class DbConfigs
     {
+        private static object lockHelper = new object();
+
+        private static DbConfigInfo m_configinfo;
+
+        private static string configpath = Utils.GetMapPath("~/NSWindBase.config");
+        /// <summary>
+        /// 静态构造函数初始化相应实例和定时器
+        /// </summary>
+        static DbConfigs()
+        {
+            if (m_configinfo == null)
+            {
+                m_configinfo = Load();
+            }
+        }
+
+
+        /// <summary>
+        /// 重设配置类实例
+        /// </summary>
+        public static void ResetConfig()
+        {
+            m_configinfo = Load();
+        }
+        /// <summary>
+        /// 取得静态配置信息
+        /// </summary>
+        /// <returns></returns>
+        public static DbConfigInfo GetConfig()
+        {
+            return m_configinfo;
+        }
+
+        
+        /// <summary>
+        /// 序列化配置信息为XML
+        /// </summary>
+        /// <param name="configinfo">配置信息</param>
+        /// <param name="configFilePath">配置文件完整路径</param>
+        public static void Save(DbConfigInfo configinfo)
+        {
+            lock (lockHelper)
+            {
+                SerializationHelper.SaveXml(configinfo, configpath);
+            }
+        }
+
+        /// <summary>
+        /// 从XML加载配置信息
+        /// </summary>
+        /// <returns></returns>
+        public static DbConfigInfo Load()
+        {
+            return (DbConfigInfo)SerializationHelper.LoadXml(typeof(DbConfigInfo), configpath);
+        }
+
+        #region 过时代码
+        [Obsolete("过时的")]
         /// <summary>
         /// 得到数据库连接字符串
         /// </summary>
@@ -17,11 +74,12 @@ namespace Natsuhime.Data
             //return @"Data Source=D:\database\sqlite\aspx163.config";
             return ConfigurationSettings.AppSettings["dbconnstring"];
         }
-
+        [Obsolete("过时的")]
         public static string GetDbType()
         {
             //return "Sqlite";
             return ConfigurationSettings.AppSettings["dbtype"];
         }
+        #endregion
     }
 }
