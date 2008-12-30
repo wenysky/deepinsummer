5e181496754cd36f0aa73ffd3418f390e97b32b8
wenysky <wenysky@b489ef22-979d-11dd-bbfe-3dabce05a288>
2008-12-30 10:58:51 +0000

    增加MD5验证

--- DeepinSummer/Common/Utils.cs
+++ DeepinSummer/Common/Utils.cs
@@ -60,5 +60,31 @@ namespace Natsuhime.Common
             string timeStamp = toNow.Ticks.ToString();
             return timeStamp.Substring(0, timeStamp.Length - 7);
         }
+        /// <summary>
+        /// MD5加密(32位)
+        /// </summary>
+        /// <param name="str">原字符串</param>
+        /// <returns>32位加密后的字符串</returns>
+        public static string MD5(string str)
+        {
+            return MD5(str, false);
+        }
+        /// <summary>
+        /// MD5加密(32/16位)
+        /// </summary>
+        /// <param name="str">原字符串</param>
+        /// <param name="cry16">是否采用16位加密方式</param>
+        /// <returns>加密后的字符串</returns>
+        public static string MD5(string str, bool cry16)
+        {
+            if (cry16) //16位MD5加密（取32位加密的9~25字符） 
+            {
+                return System.Web.Security.FormsAuthentication.HashPasswordForStoringInConfigFile(str, "MD5").Substring(8, 16);
+            }
+            else//32位加密 
+            {
+                return System.Web.Security.FormsAuthentication.HashPasswordForStoringInConfigFile(str, "MD5");
+            }
+        }
     }
 }
