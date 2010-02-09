16f845129b37ef9aa81093f63f12950766d43c42
wenysky <wenysky@b489ef22-979d-11dd-bbfe-3dabce05a288>
2010-02-09 05:32:35 +0000

    增加文本分析url模块

---
+++ DeepinSummer/Web/Plugin/Discuz/TextAnalyze.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Natsuhime.Web.Plugin.Discuz
+{
+    public class TextAnalyze
+    {
+        public static Dictionary<string, string> GetThreadsInBoard(string sourceHtml)
+        {
+            Dictionary<string, string> threads = new Dictionary<string, string>();
+
+            string regexstring = RegexStringLib.GetThreadsInBoard();
+            MatchCollection mc = RegexUtility.GetMatchFull(sourceHtml, regexstring);
+
+            if (mc != null)
+            {
+                foreach (Match m in mc)
+                {
+                    threads.Add(m.Groups[2].Value, m.Groups[1].Value);
+                }
+            }
+
+            return threads;
+        }
+    }
+}
